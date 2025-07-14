23d12b752246de467b896ce165e7c89291af3063
Hazem Adel <hadel6464@gmail.com>
2025-07-14 23:17:24 +0200

    learn.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// start 
+console.log("hallo");
+console.table(["hazem" , "adel" ,"gaber"]);
+// Data Types And Typeof Operator
+console.log(typeof "hazem"); 
+console.log(typeof 100);
+console.log(typeof ["hazem" , "adel", "gaber"]);
+console.log(typeof {name: "hazem" , age: 24 , country: "Egypt"});
+console.log(typeof true);
+console.log(typeof undefined);
+//  Variables Introduction
+var user = "hazem";
+var age = 24 ;
+var country = 'Egypt';
+console.log(user);
+console.log(age);
+console.log(country);
